a4813f81fc6c2cb1fe0cb03079edfb7f3d38a113
JJ Herasymchuk <evhenii.herasymchuk@gmail.com>
2018-05-14 21:42:09 +0200

    Initial setup with react-create-app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        ToDo app with create-react-app and react-transition-group
+      </div>
+    );
+  }
+}
+
+export default App;
